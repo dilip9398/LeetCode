27caefcd3f02800ac6ef27b6699f03fc7901acbf
Kancherla Dilip <138967561+dilip9398@users.noreply.github.com>
2025-07-08 20:21:28 +0530

    Time: 1711 ms (66.67%), Space: 65.5 MB (77.78%) - LeetHub

---
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def maxValue(self, events, k):
+        """
+        :type events: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
+        events.sort(key=lambda ans:ans[1])
+        dp=[[0,0]]
+        dp2=[[0,0]]
+        for x in range(k):
+            for s,e,v in events:
+                i=bisect.bisect(dp,[s])-1
+                if dp[i][1]+v>dp2[-1][1]:
+                    dp2.append([e,dp[i][1]+v])
+
+            dp=dp2
+            dp2=[[0,0]]
+
+        return dp[-1][-1]   
+        
