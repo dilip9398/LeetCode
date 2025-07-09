755a546389e86b61de1913802887ed2409421135
Kancherla Dilip <138967561+dilip9398@users.noreply.github.com>
2025-07-09 20:25:02 +0530

    Time: 11 ms (51.49%), Space: 13.7 MB (31.18%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 class Solution(object):
     def majorityElement(self, nums):
         # count = 0
@@ -9,6 +10,21 @@ class Solution(object):
         #     count += (1 if num == element else -1)
         # return element
 
+        # n = len(nums)
+        # nums.sort()
+        # return nums[n//2]
+
+        #Using the Hashmap
+
         n = len(nums)
-        nums.sort()
-        return nums[n//2]
+        n //= 2
+
+        hashmap = defaultdict(int)
+
+        for num in nums:
+            hashmap[num] += 1
+
+        for key, value in hashmap.items():
+            if value > n:
+                return key
+        return 0
