6502d5b64381c49fd77c778c73d7d6ad6d04ebb1
Kancherla Dilip <138967561+dilip9398@users.noreply.github.com>
2025-07-11 20:51:38 +0530

    Time: 514 ms (56.87%), Space: 12.5 MB (23.04%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -4,9 +4,9 @@ class Solution(object):
         n = len(nums)
         result = []
         nums.sort()
-        for i in range(n):
+        for i in range(n-3):
             if i > 0 and nums[i] == nums[i -1] :continue
-            for j in range(i+ 1, n):
+            for j in range(i+ 1, n-2):
                 if j != i + 1 and nums[j] == nums[j - 1]: continue
 
                 k = j + 1
