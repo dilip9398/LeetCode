aaa929620862ce0a03aa04c21a30c44189309b0d
Kancherla Dilip <138967561+dilip9398@users.noreply.github.com>
2025-07-11 23:17:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def mostBooked(self, n, meetings):
+        """
+        :type n: int
+        :type meetings: List[List[int]]
+        :rtype: int
+        """
+        ready = [r for r in range(n)]
+        rooms = []
+        heapify(ready)
+        res = [0] * n
+        for s,e in sorted(meetings):
+            while rooms and rooms[0][0] >= s:
+                t,r = heappop(rooms)
+                heappush(ready, r)
+            if ready:
+                r = heappop(ready)
+                heappush(rooms, [e, r])
+            else:
+                t,r = heappop(rooms)
+                heappush(rooms, [t + e - s, r])
+            res[r] += 1
+        return res.index(max(res))
+        
