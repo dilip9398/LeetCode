a423ce91c12c628d21dd9a1f10f4c9a8e8a48dd4
Kancherla Dilip <138967561+dilip9398@users.noreply.github.com>
2025-07-12 16:14:35 +0530

    Time: 25 ms (6.88%), Space: 17.9 MB (23.93%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        n = len(nums)
+        result = []
+        frequency = Counter(nums)
+
+        for k , v in frequency.items():
+            if v > n//3:
+                result.append(k)
+        
+        result.sort()
+        return result
