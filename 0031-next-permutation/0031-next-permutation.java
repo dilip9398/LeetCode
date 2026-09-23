b9c095e26eb6b17e850cd40f44ef4a27fad76f9c
Kancherla Dilip <138967561+dilip9398@users.noreply.github.com>
2026-09-23 21:54:34 +0530

    Time: 0 ms (100%), Space: 44.6 MB (69.34%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,31 +1,31 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int n=nums.length;
-        int p=-1;
-        for(int i=n-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                p=i;
+        int n = nums.length;
+        int p = -1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                p = i;
                 break;
             }
         }
-        if(p!=-1){ 
-        for(int i=n-1;i>=0;i--){
-            if(nums[i]>nums[p]){
-                int temp=nums[p];
-                nums[p]=nums[i];
-                nums[i]=temp;
-                break;
+        if (p != -1) {
+            for (int i = n - 1; i >= 0; i--) {
+                if (nums[i] > nums[p]) {
+                    int temp = nums[p];
+                    nums[p] = nums[i];
+                    nums[i] = temp;
+                    break;
+                }
             }
         }
-        }
-        int l=p+1;
-        int r=n-1;
-        while(l<r){
-           int temp=nums[l];
-           nums[l]=nums[r];
-           nums[r]=temp;
-           l++;
-           r--;
+        int l = p + 1;
+        int r = n - 1;
+        while (l < r) {
+            int temp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = temp;
+            l++;
+            r--;
         }
     }
 }
